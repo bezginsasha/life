f26cc9795a72b56c6f724290d037827e6f9851aa
bezginsasha <bezgin.sasha06@gmail.com>
2021-06-13 13:09:40 +0700

    function to read image and translate it to matrx list of boolean

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ def is_there_life(pixel):
 	else return false
 	"""
 	return pixel[0] == 0
+
+def read_image_to_array():
+	"""
+	function read all pixels in default.png image
+	and every black pixel understands as live cell
+	then function create matrix list of boolean and returns it
+	"""
+	img = Image.open('default.png')
+	res_arr = []
+
+	for y in range(img.height):
+		res_arr.append([])
+
+		for x in range(img.width):
+			pixel = img.getpixel((x,y))
+			res_arr[y].append(is_there_life(pixel))
+	return res_arr
