bd6c8075131dc2be4e894191cb1a18551bb6a58a
bezginsasha <bezgin.sasha06@gmail.com>
2021-06-13 09:31:15 +0700

    test commit to update character in cmd by position
    need command:
    pip install windows-curses
    because currently use windows

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import sys
+import curses
 import time
 
-for i in range(50):
-	sys.stdout.write(f'\r{i}')
-	sys.stdout.flush()
-	time.sleep(0.2)
+stdscr = curses.initscr()
+win_width = curses.COLS
+win_height = curses.LINES
+
+stdscr.addstr(0,0,'$')
+stdscr.addstr(1,1,'$')
+stdscr.addstr(2,2,'$')
+stdscr.refresh()
+time.sleep(2)
+
+stdscr.addstr(0,0,'%')
+stdscr.addstr(1,1,'%')
+stdscr.addstr(2,2,'%')
+stdscr.refresh()
+time.sleep(2)
