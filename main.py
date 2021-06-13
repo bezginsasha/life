f23355c3783c2b134cbbe8c257702e0da1c63ca9
bezginsasha <bezgin.sasha06@gmail.com>
2021-06-13 13:06:03 +0700

    function to detect is there life in concrete pixel

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from PIL import Image
 
 arr = []
+
+def is_there_life(pixel):
+	"""
+	function get pixel from image
+	if this pixel is black, function returns true
+	else return false
+	"""
+	return pixel[0] == 0
